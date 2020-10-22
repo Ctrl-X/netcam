710c4b82ffd87d97c6d2e0a5edcacb693d71aba9
CtrlX <julien@consoshop.com>
2020-10-22 09:55:04 -0400

    server cleanupd

--- client.py
+++ client.py
@@ -8,29 +8,27 @@ if __name__ == '__main__':
     print(netCam.getDetail())
     netCam.startClient()
 
-
-    while netCam.isRunning:
-
-        key = cv2.waitKey(1)
-        if key != -1:
-            if key == ord('q'):  # q to quit
-                netCam.clearAll()
-            elif key == ord('d'):  # d to show debug
-                netCam.toggleDebug()
-            elif key == 190:  # F1
-                netCam.setDisplayResolution('QVGA')
-            elif key == 191:  # F2
-                netCam.setDisplayResolution('VGA')
-            elif key == 192:  # F3
-                netCam.setDisplayResolution('HD')
-            elif key == 193:  # F4
-                netCam.setDisplayResolution('FHD')
-            elif key == 194:  # F5 to toggle fullscreen
-                netCam.toggleFullScreen()
-            else:
-                print(f'Key pressed: {key}')
-        netCam.display()
-
-
-    netCam.clearAll()
+    try:
+        while netCam.isRunning:
+            key = cv2.waitKey(1)
+            if key != -1:
+                if key == ord('q'):  # q to quit
+                    netCam.clearAll()
+                elif key == ord('d'):  # d to show debug
+                    netCam.toggleDebug()
+                elif key == 190:  # F1
+                    netCam.setDisplayResolution('QVGA')
+                elif key == 191:  # F2
+                    netCam.setDisplayResolution('VGA')
+                elif key == 192:  # F3
+                    netCam.setDisplayResolution('HD')
+                elif key == 193:  # F4
+                    netCam.setDisplayResolution('FHD')
+                elif key == 194:  # F5 to toggle fullscreen
+                    netCam.toggleFullScreen()
+                else:
+                    print(f'Key pressed: {key}')
+            netCam.display()
+    except KeyboardInterrupt:
+        netCam.clearAll()
     exit()
