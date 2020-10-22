4c750f985ac48dfd539be92972c5eb8a174ab6cd
CtrlX <julien@consoshop.com>
2020-10-22 12:14:10 -0400

    WIP : fullscreen

--- NetCam.py
+++ NetCam.py
@@ -309,12 +309,12 @@ class NetCam:
             textPosX, textPosY = NetCam.TEXT_POSITION
             textPosX += int(40 * debugTextSize)
             textPosY += int(40 * debugTextSize)
-            frame = cv2.putText(frame, f'Capture : {debugTextSize} fps ({self.captureResolution})',
+            frame = cv2.putText(frame, f'Capture : {self.captureFps.fps} fps ({self.captureResolution})',
                                 (textPosX, textPosY),
                                 cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR, thickness,
                                 cv2.LINE_AA)
             textPosY += int(40 * debugTextSize)
-            frame = cv2.putText(frame, f'Display : {thickness} fps ({self.displayResolution})',
+            frame = cv2.putText(frame, f'Display : {self.displayFps.fps} fps ({self.displayResolution})',
                                 (textPosX, textPosY), cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR,
                                 thickness,
                                 cv2.LINE_AA)
@@ -322,19 +322,6 @@ class NetCam:
             frame = cv2.putText(frame, f'Network : {self.networkFps.fps} fps', (textPosX, textPosY),
                                 cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR, thickness,
                                 cv2.LINE_AA)
-            # frame = cv2.putText(frame, f'Capture : {self.captureFps.fps} fps ({self.captureResolution})',
-            #                     (textPosX, textPosY),
-            #                     cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR, thickness,
-            #                     cv2.LINE_AA)
-            # textPosY += int(40 * debugTextSize)
-            # frame = cv2.putText(frame, f'Display : {self.displayFps.fps} fps ({self.displayResolution})',
-            #                     (textPosX, textPosY), cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR,
-            #                     thickness,
-            #                     cv2.LINE_AA)
-            # textPosY += int(40 * debugTextSize)
-            # frame = cv2.putText(frame, f'Network : {self.networkFps.fps} fps', (textPosX, textPosY),
-            #                     cv2.FONT_HERSHEY_SIMPLEX, debugTextSize, NetCam.TEXT_COLOR, thickness,
-            #                     cv2.LINE_AA)
         cv2.imshow(NetCam.DEFAULT_WINDOW_NAME, frame)
 
     def toggleDebug(self):
