324b16e985f5432624af5e12e8cd7fd912ece0a0
CtrlX <julien@consoshop.com>
2020-10-22 09:57:39 -0400

    server cleanupd

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from NetCam import *
 
 if __name__ == '__main__':
     print('Started main.py...')
-    netCam = NetCam(display='QVGA')
+    netCam = NetCam(display='QVGA', capture='VGA')
     print(netCam.getDetail())
     netCam.startClient()
 
