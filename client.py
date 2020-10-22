2685ce4f88261019362594398eec182031275c7c
CtrlX <julien@consoshop.com>
2020-10-22 12:18:42 -0400

    WIP : fullscreen

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from NetCam import *
 
 if __name__ == '__main__':
     print('Started main.py...')
-    netCam = NetCam(display='QVGA', capture='FHD')
+    netCam = NetCam(display='FHD', capture='FHD')
     print(netCam.getDetail())
     netCam.startClient()
 
