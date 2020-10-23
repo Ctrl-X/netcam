c021f52b34fabb31f956c515696caf761be15815
CtrlX <julien@consoshop.com>
2020-10-23 14:49:23 -0400

    network with ZMQ

--- client.py
+++ client.py
@@ -4,7 +4,8 @@ from NetCam import *
 
 if __name__ == '__main__':
     print('Started main.py...')
-    netCam = NetCam(display='VGA', capture='VGA',isstereocam=True,flipvertical= True)
+    netCam = NetCam(display='VGA', capture='VGA',isstereocam=True)
+    netCam.invertVertical()
     netCam.startClient()
 
     try:
