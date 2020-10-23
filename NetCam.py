1b0887789eaa14c02b5cfbab117037fde09cb084
CtrlX <julien@consoshop.com>
2020-10-22 22:10:58 -0400

    WIP : Networking

--- NetCam.py
+++ NetCam.py
@@ -106,7 +106,7 @@ class NetCam:
                 self.networkFps.compute()
             # socket.send(self.imgBuffer)
             messagedata = random.randrange(1,215) - 80
-            print "%d %d" % (topic, messagedata)
+            print("%d %d" % (topic, messagedata))
 
             socket.send("%d %d" % (topic, messagedata))
             # i += 1
