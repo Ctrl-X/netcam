9cd8a877c65ec7653223e44f65f7bf63f69f9bae
CtrlX <julien@consoshop.com>
2020-10-22 22:11:30 -0400

    WIP : Networking

--- NetCam.py
+++ NetCam.py
@@ -108,7 +108,7 @@ class NetCam:
             messagedata = random.randrange(1,215) - 80
             print("%d %d" % (topic, messagedata))
 
-            socket.send("%d %d" % (topic, messagedata))
+            socket.send_string("%d %d" % (topic, messagedata))
             # i += 1
             time.sleep(0.001)
         self.console('Network thread stopped.', 1)
